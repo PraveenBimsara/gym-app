9b4e75481d99b177a359901f50cbfe8387eb6956
PraveenBimsara <praveenbimsara99@gmail.com>
2024-07-09 21:29:08 +0530

    setup enums for navbar

--- empower/src/App.tsx
+++ empower/src/App.tsx
@@ -1,8 +1,10 @@
 import Navbar from "@/scenes/navbar";
 import { useState } from "react";
+import { SelectedPage } from "@/shared/types";
+
 
 function App() {
-const [selectedPage,setSelectedPage] = useState<string>('homes')
+const [selectedPage,setSelectedPage] = useState<SelectedPage>(SelectedPage.Home)
   return (
     <div className='app bg-gray-20'>
       <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
--- empower/src/scenes/navbar/index.tsx
+++ empower/src/scenes/navbar/index.tsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
 import {Bars3Icon, XMarkIcon} from "@heroicons/react/24/solid";
 import Link from "./Link";
+import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
 
 type Props = {
-    selectedPage:string;
-    setSelectedPage:(value:string)=>void;
+    selectedPage:SelectedPage;
+    setSelectedPage:(value:SelectedPage)=>void;
 }
 
 const Navbar = ({selectedPage,setSelectedPage}: Props) => {
     const flexBetween = "flex items-center justify-between";
+    const isAboveMediumScreens = useMediaQuery("(min-width:1060px)");
+    const [isMenuToggled, setIsMenuToggled] = useState<boolean>(false);
+
   return <nav>
     <div className={`${flexBetween} fixed top-0 z-30 w-full py-6`}>
         <div className={`${flexBetween} mx-auto w-5/6`}>
@@ -17,6 +23,7 @@ const Navbar = ({selectedPage,setSelectedPage}: Props) => {
               <h1 className="text-xl font-bold">EMPOWER</h1>
 
                 {/**Right Side */}
+                {isAboveMediumScreens ? (
                 <div className={`${flexBetween} w-full`}>
                     <div className={`${flexBetween} gap-8 text-sm`}>
                         <Link page="Home" selectedPage={selectedPage} setSelectedPage={setSelectedPage}/>
@@ -26,12 +33,19 @@ const Navbar = ({selectedPage,setSelectedPage}: Props) => {
                     </div>
                     <div className={`${flexBetween} gap-8`}>
                         <p>Sign In</p>
-                        <button>Become a Member</button>
+                        <ActionButton setSelectedPage={setSelectedPage}>Become a Member</ActionButton>
                     </div>
-                </div>
+                </div> )
+                : (
+                    <button className="rounded-full bg-secondary-500 p-2" onClick={()=>setIsMenuToggled(!isMenuToggled)}>
+                        <Bars3Icon className="h-6 w-6 text-white"/>
+                    </button>
+                )}
             </div>
         </div>
     </div>
+    {/* Mobile Menu Model */}
+    
   </nav>;
 }
 
